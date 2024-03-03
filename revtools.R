de030242e6a95f69cab5c5deea85fca036b21d00
Dr. Manika Lamba <lambamanika07@gmail.com>
2024-03-03 16:55:57 -0600

    merged file

--- revtools.R
+++ revtools.R
@@ -6,26 +6,41 @@
 library (revtools)
 library(RefManageR)
 library(bibliometrix)
+library(bibtex)
 
-# merging bib files from wos and scopus
-# Read .bib files from Web of Science and Scopus
+# read web of science file
 
-wos_bib <- ReadBib("savedrecs.bib")
+file <- "wos.bib"
 
+M <- convert2df(file, dbsource = "wos", format = "bibtex")
+
+head(M["TC"])
+
+# load pubmed file
+pubmed <- "pubmed.txt"
+
+P <- convert2df(pubmed, dbsource = "pubmed", format = "pubmed")
+
+head(M["TC"])
 
 #scopus_bib <- ReadBib("scopus.bib")
 
 # Merge the two lists of references
-merged_bib <- c(wos_bib, scopus_bib)
+#merged_bib <- c(wos_bib, scopus_bib)
 
 # Remove duplicates based on DOI (assuming DOI is present in both .bib files)
-unique_bib <- unique(merged_bib, by = "DOI")
+#unique_bib <- unique(merge, by = "DOI")
+
+Q<-mergeDbSources(M,P,remove.duplicated=TRUE) 
+
 
 # Write the merged and deduplicated .bib file
-WriteBib(unique_bib, file = "merged_and_deduplicated2.bib")
+#WriteBib(unique_bib, file = "merged_and_deduplicated2.bib")
+
+write.csv(Q, "merged.csv")
 
 # read file
-data <- read_bibliography("merged_and_deduplicated.bib")
+data <- read_bibliography("")
 
 # distribute tasks
 task <- distribute_tasks(data,reviewers,write_csv=TRUE, file_name="reviewer.csv",return_data=FALSE,...)
